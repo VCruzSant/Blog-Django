58b6f9a622a05b7a744cb06f1ddb7f613b509c1e
VCruzSant <sviniantiago@gmail.com>
2023-12-22 14:40:40 -0300

    Creating site setup

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from site_setup.models import MenuLink
+from site_setup.models import MenuLink, SiteSetup
 # Register your models here.
 
 
@@ -8,3 +8,13 @@ class MenuLinkAdmin(admin.ModelAdmin):
     list_display = 'id', 'text', 'url_or_path',
     list_display_links = 'id', 'text', 'url_or_path',
     search_fields = 'id', 'text', 'url_or_path',
+
+
+@admin.register(SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
+    list_display = 'title', 'description',
+
+    # permissão para adicionar:
+    def has_add_permission(self, request):
+        # retorna False se existir algum obj já criado, para ter apenas 1 setup
+        return not SiteSetup.objects.exists()
