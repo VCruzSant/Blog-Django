967d35a5b7275636674f89b708b6fe2b6e0615cb
VCruzSant <sviniantiago@gmail.com>
2024-01-05 15:21:05 -0300

    Created new pages

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -11,3 +11,23 @@ def index(request):
             'name': 'Vini Sant'
         }
     )
+
+
+def post(request):
+    return render(
+        request,
+        'blog/pages/post.html',
+        {
+            'name': 'Esse é o POST'
+        }
+    )
+
+
+def page(request):
+    return render(
+        request,
+        'blog/pages/page.html',
+        {
+            'name': 'Essa é a PAGE'
+        }
+    )
